f2feed10390ea7c3fd4766697eaf096d44fc3977
Evgeny Eremin <ereminevgeny78@gmail.com>
2023-03-06 14:51:09 +0700

    reset to prev ver

--- src/App.js
+++ src/App.js
@@ -3,35 +3,13 @@ import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import Cards from "./components/Cards/Cards";
-import Tree from "./components/Tree/Tree";
 
 function App() {
-  const [cardsToRender, setCardsToRender] = useState(false);
-  const [treeToRender, setTreeToRender] = useState(false);
-
   return (
     <div className="app">
       <Header />
-      <div className="app__variants">
-        {!cardsToRender? <h1>Choose how to represent data</h1> : null}
-      <button
-        onClick={() => {
-          setCardsToRender((prevState) => !prevState);
-        }}
-      >
-        CARDS
-      </button>
-      {cardsToRender ? <Cards /> : null}
+      <Cards />
 
-      <button
-        onClick={() => {
-          setTreeToRender((prevState) => !prevState);
-        }}
-      >
-        TREE
-      </button>
-      {treeToRender ? <Tree /> : null}
-      </div>
       <Footer />
     </div>
   );
